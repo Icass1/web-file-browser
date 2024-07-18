7fdf1b4d730a3162c86024e67061a63e6290e32f
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-07-18 17:50:36 +0200

    Added main component

---
+++ client/src/components/Main.js
@@ -0,0 +1,7 @@
+export default function Main() {
+    return (
+        <div className="bg-blue-950 h-full">
+            <label className="block relative text-center w-full top-1/2 font-bold text-white text-9xl -translate-y-1/2">File browser</label>
+        </div>
+    )
+}
