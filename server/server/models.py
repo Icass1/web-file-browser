af2f9b791e650635833a20c57239f7d82955ab01
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-07-21 12:00:50 +0200

    Fixed User model

--- server/server/models.py
+++ server/server/models.py
@@ -1,16 +1,33 @@
 
-
 from flask import current_app
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import bcrypt
 
-db = current_app.config["database"]
+db = current_app.config["db"]
 
-class User(db.Model):
-    id =                db.Column(db.String(30), primary_key=True, unique=True)
-    username =          db.Column(db.String(80), unique=True, nullable=False)
-    password =          db.Column(db.String(80), unique=True, nullable=False)
-    scope =             db.Column(db.String(255))
-    admin =             db.Column(db.Boolean, default=False)
-    shares =            db.Column(db.String(20000), default="[]")
+class User(UserMixin, db.Model):
+    id =               db.Column(db.String(30), primary_key=True, unique=True)
+    username =         db.Column(db.String(80), unique=True, nullable=False)
+    password_hash =    db.Column(db.String(128), unique=True, nullable=False)
+    salt =             db.Column(db.String(29))
+    scope =            db.Column(db.String(255), default="")
+    admin =            db.Column(db.Boolean, default=False)
+    shares =           db.Column(db.String(20000), default="[]")
+
+    def set_password(self, password):
+        # Generate a random salt and hash the password
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        self.password_hash = hashed_password
+        self.salt = salt
+
+    def check_password(self, password):
+        if bcrypt.checkpw(password.encode('utf-8'), self.password_hash):
+            db.session.commit()
+            return True
+        else: 
+            return False
+
 
 class Shares(db.Model):
     id =               db.Column(db.String(30), primary_key=True, unique=True)
